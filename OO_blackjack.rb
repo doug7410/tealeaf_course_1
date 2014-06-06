8ed8dc7ad96817206e907ed403f5fd39b4a5b6ea
Doug Steinberg <dstein-phins@hotmail.com>
2014-06-06 04:00:17 +0000

    refactored code, added betting, made cards look better

--- OO_blackjack.rb
+++ OO_blackjack.rb
@@ -3,16 +3,13 @@
 
 # CARDS///////////////////////////////////////////////////////////////
 class Card
-  attr_accessor :value, :suit, :nice_card
+  attr_accessor :value, :suit, :formated_value, :formated_suit
 
   def initialize(v,s)
     @value = v
     @suit = s
-    @nice_card = "
-    -----
-    |#{pretty_value(@value)} | 
-    | #{pretty_suit(@suit)} |
-    -----"
+    @formated_value = "|#{pretty_value(@value)} | "
+    @formated_suit = "| #{pretty_suit(@suit)} | "
   end
 
   def pretty_suit(suit)
@@ -31,7 +28,6 @@ class Card
       "10"
     end
   end
-
 end
 
 # DECK////////////////////////////////////////////////////////////////
@@ -58,61 +54,39 @@ class Deck
   end
 end
 
-# TABLE///////////////////////////////////////////////////////////////
-class Table
-  
-  attr_accessor :player, :dealer
-
-  def initialize(p, d)
-    @player = p
-    @dealer = d
-  end
-
-  def display_cards(player_stand = false)
-    system "clear"
-    puts "Hi #{player.name}!"
-    puts "Rules: (1) Dealer hits under 17 (2)We are playing with four decks \n\n"
-    puts "Let's get started! \n\n" 
-    puts "-------------------------------------"
-    puts "Dealer Cards:\n" 
-    
-    if !player_stand
-      puts dealer.hand[0].nice_card 
-      puts "
-    -----
-    |***|
-    |***|
-    -----"
-    else
-      display = ''
-      dealer.hand.each do |card|
-        display += card.nice_card 
-      end
-      puts display
-      puts "\nfor a total of #{dealer.calc_cards(dealer.hand)}\n"
-    end
-    
-    puts "-------------------------------------\n"
-    puts "#{player.name}'s Cards:\n" 
-    
-    display = ''
-    player.hand.each do |card|
-      display += card.nice_card  
-    end
-    puts display
-    
-    puts "for a total of #{player.calc_cards(player.hand)}\n\n"
-    puts "-------------------------------------\n\n"
-  end
-end
-
 
 # HAND////////////////////////////////////////////////////////////////
 module Hand
 
-  def calc_cards(hand)
+  def show_cards
+    top_of_card = ''
+    card_value = ''
+    card_suit = ''
+    bottom_of_card = ''
+    self.hand.each { top_of_card += "----- " }
+    self.hand.each { bottom_of_card += "----- " }
+    self.hand.each { |card| card_value += card.formated_value } 
+    self.hand.each { |card| card_suit += card.formated_suit }  
+    puts top_of_card
+    puts card_value
+    puts card_suit
+    puts bottom_of_card
+  end
+
+  def show_dealer_flop
+    top_of_card = "----- -----" 
+    bottom_of_card = "----- -----" 
+    card_value = self.hand[0].formated_value + "|***|" 
+    card_suit = self.hand[0].formated_suit  + "|***|"
+    puts top_of_card
+    puts card_value
+    puts card_suit
+    puts bottom_of_card
+  end
+
+  def calc_cards
     @total = 0
-    hand.each do |card|
+    self.hand.each do |card|
       if card.value.to_i > 0 # if the card is a number
         @total += card.value
       elsif ["J", "Q", "K"].include?(card.value) #if the card is J, Q, or K
@@ -131,13 +105,15 @@ end
 
 # PLAYER//////////////////////////////////////////////////////////////
 class Player
-  attr_accessor :name, :hand
+  attr_accessor :name, :hand, :chips, :bet
 
   include Hand
 
   def initialize
     @name = ''
     @hand = []
+    @chips = 500
+    @bet = ''
   end
 
   def player_action
@@ -164,41 +140,18 @@ end
 # BLACKJACK GAME/////////////////////////////////////////////////////
 class Blackjack
 
-  attr_accessor :deck, :player, :dealer, :table, :game_count
+  attr_accessor :deck, :player, :dealer, :game_count
+
+  BLACKJACK_AMOUNT = 21
+  DEALER_STAY_AMMOUNT = 17
 
   def initialize 
     @player = Player.new
     @dealer = Dealer.new
-    @table = Table.new(player,dealer)
     @game_count = 0
   end
 
-  def start_game
-
-    if game_count > 0
-      system 'clear'
-      puts 're-shuffling the cards'
-      sleep 1
-    end
-
-    @deck = Deck.new(4)
-    system 'clear'
-    if self.game_count == 0
-      puts "Hi, what's your name?"
-      player.name = gets.chomp
-    end
-    player.hand = []
-    player.hand.push( deck.deal_card )
-    player.hand.push( deck.deal_card )
-    dealer.hand = []
-    dealer.hand.push( deck.deal_card )
-    dealer.hand.push( deck.deal_card )
-    table.display_cards
-    player_turn
-  end
-
   def player_turn
-
     @action = ''
     
     #check for Blackjack
@@ -211,81 +164,197 @@ class Blackjack
       while !['h', 's'].include?(@action)
         puts "Sorry I didn't get that. Plase enter h for hit or s for stay."
         @action = gets.chomp
+      
       end
     end
     
     while @action == 'h' and !@blackjack
       player.hand.push( deck.deal_card )
       sleep 0.5
-      table.display_cards
+      display_cards
       break if check_result(@action)
       @action = player.player_action
     end
-    
-    if @action == 's'
-      #check for Blackjack
-      if player.calc_cards(player.hand) == 21
-        "dealer has Blackjack"
-      end
-      dealer_turn
-    end
-    
   end
 
   def dealer_turn
-    table.display_cards(true)
+    display_cards(true)
+
+    #check for Blackjack
+    if dealer.calc_cards == BLACKJACK_AMOUNT
+      "Sorry, the dealer has Blackjack"
+    end
     
-    while dealer.calc_cards(dealer.hand) < 17
+    while dealer.calc_cards < DEALER_STAY_AMMOUNT and !player_bust
       sleep 0.5
       dealer.hand.push( deck.deal_card )
-      table.display_cards(true)
+      display_cards(true)
     end
     
     check_result(@action)
   end
 
+  def player_bust
+    if player.calc_cards > BLACKJACK_AMOUNT
+      true
+    end
+  end
+
   def check_result(action = false)
-    @player_total = player.calc_cards(player.hand)
-    @dealer_total = dealer.calc_cards(dealer.hand)
+    player_total = player.calc_cards
+    dealer_total = dealer.calc_cards
     
-    if player.hand.count == 2 and @player_total == 21
-      table.display_cards(true)
-      puts "You have Blackjack!"
+    if player.hand.count == 2 and player_total == BLACKJACK_AMOUNT
+      display_cards(true)
+      puts "You have Blackjack! You win #{player.bet.to_i * 2} chips!"
+      add_chips
       return 'blackjack'
-    elsif @player_total > 21
-      table.display_cards(true)
+    elsif player_total > BLACKJACK_AMOUNT
+      display_cards(true)
       puts "Sorry, you bust. Better luck next time"
       true
-    elsif @player_total == 21
-      table.display_cards(true)
-      puts "You win with 21!"
+    elsif player_total == BLACKJACK_AMOUNT
+      display_cards(true)
+      puts "You win #{player.bet.to_i * 2} chips!"
+      add_chips
       true
-    elsif @player_total >  @dealer_total and @action == 's'
-      puts "You beat the dealer. You win!"
+    elsif player_total >  dealer_total and @action == 's'
+      puts "You beat the dealer. You win #{player.bet.to_i * 2} chips!"
+      add_chips
       true
-    elsif @dealer_total > 21 and @action == 's'
-      puts "The dealer bust. You win!"
+    elsif dealer_total > BLACKJACK_AMOUNT and @action == 's'
+      puts "The dealer bust. You win #{player.bet.to_i * 2} chips!"
+      add_chips
       true
-    elsif @dealer_total > @player_total and @action == 's'
+    elsif dealer_total > player_total and @action == 's'
       puts "Sorry, the dealer wins this one, better luck next time."
       true
-    elsif @dealer_total == @player_total and @action == 's'
+    elsif dealer_total == player_total and @action == 's'
       puts "It's a push."
       true
     end
+  end
 
+  def add_chips
+    winnings = player.bet.to_i * 2
+    player.chips += winnings 
+    puts "You now have #{player.chips} chips"
+  end
+
+  def deduct_chips
+     player.chips -= (player.bet.to_i)
+  end
+
+  def check_bet
+    while player.bet.to_i == 0 || player.bet.to_i > player.chips
+      puts "Please enter a number #{player.chips} or less."
+      player.bet = gets.chomp.to_i
+    end   
+  end
+
+  def shuffle_deck
+    if self.game_count > 0
+      system 'clear'
+      puts 'shuffling the cards'
+      sleep 0.7
+    end
+      @deck = Deck.new(4)
+  end
+
+  def greet_player
+    system 'clear'
+    if self.game_count == 0
+      puts "\n---Hi, Welcome to Blackjack! What's your name?---\n\n"
+      player.name = gets.chomp
+      puts "\nNice to meet you #{player.name}. You have #{player.chips} chips.\n\n"
+      puts "How much would you like to bet?"
+      player.bet = gets.chomp
+      check_bet
+      deduct_chips
+    else
+      if player.chips > 0
+        puts "\n---You have #{player.chips} chips, how much would you like to bet?---\n\n"
+      else
+        puts "\n---You have no more chips! Go home.---"
+        exit
+      end
+      player.bet = gets.chomp
+      check_bet
+      deduct_chips
+    end
+  end
+
+
+  def display_cards(player_stand = false)
+    
+    system "clear"
+    if game_count == 0
+      puts "Hi #{player.name}!\n\n"
+      puts "Let's get started! \n\n"
+    else
+      puts "Welcome back #{player.name}"
+      puts "We're on round number #{game_count + 1}"
+    end
+    puts "Rules: (1)Dealer hits under 17 (2)If the round is a push, the dealer wins \n\n"
+    draw_line
+    puts "Dealer's Cards:\n" 
+
+    if !player_stand
+      puts "#{dealer.show_dealer_flop}\n"
+    else
+      dealer.show_cards
+      puts "for a total of #{dealer.calc_cards}\n"
+    end
+    
+    draw_line
+    puts "#{player.name}'s cards,  chips: #{player.chips}, current bet #{player.bet}\n\n" 
+    player.show_cards
+    
+    puts "\nfor a total of #{player.calc_cards}\n\n"
+    draw_line
+  end
+
+  def draw_line
+    puts "-------------------------------------\n\n"
+  end
+
+  def deal_cards
+    player.hand = []
+    player.hand.push( deck.deal_card )
+    player.hand.push( deck.deal_card )
+    dealer.hand = []
+    dealer.hand.push( deck.deal_card )
+    dealer.hand.push( deck.deal_card )
+  end
+
+  def play_again?
+    if player.chips == 0
+      puts "\n\n--- You have no more chips! Come back when you get paid again. :) ---\n\n"
+      exit
+    end
+      puts "\n\nWould you like to play again #{self.player.name}? y for yes, n for no"
+      play_again = gets.chomp
+      
+      while !['y', 'n'].include?(play_again)
+        puts "\n\nSorry I didn't get that. Plase enter y for yes or n for no."
+        play_again = gets.chomp
+      end
+
+      self.start if play_again == 'y'
+  end
+
+  def start
+    shuffle_deck      
+    greet_player
+    deal_cards
+    display_cards
+    player_turn
+    dealer_turn
+    self.game_count += 1
+    play_again?
   end 
 end
 
 game = Blackjack.new
+game.start
 
-begin
-  game.start_game
-  game.game_count += 1
-  puts "\n\nWould you like to play again #{game.player.name}? y for yes, n for no"
-  play_again = gets.chomp
-  while !['y', 'n'].include?(play_again)
-    puts "\n\nSorry I didn't get that. Plase enter y for yes or n for no."
-    play_again = gets.chomp
-  end
-end while play_again == 'y'
